f88fd2120f52b89b6c206a620a53f98eb94301f0
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2023-01-01 12:46:10 +0100

    add toggleterm options

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -1,3 +1,12 @@
+local status_ok, toggleterm = pcall(require, "toggleterm")
+if not status_ok then
+    return
+end
+
+toggleterm.setup({
+    size = 20,
+})
+
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
