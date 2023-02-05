834bb9c176cccfb041d8bbbe8a06781931efe3a8
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2023-02-05 12:34:02 +0100

    added rust diagnostics

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,12 +1,25 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
-
 lsp.ensure_installed ({
  'rust_analyzer',
  -- 'lua-language-server',
  -- 'ansible-language-server',
  -- 'jedi-language-server',
  })
+
+require('lspconfig').rust_analyzer.setup {
+    settings = {
+        ['rust-analyzer'] = {
+            checkOnSave = {
+                allFeatures = true,
+                overrideCommand = {
+                    'cargo', 'clippy', '--workspace', '--message-format=json',
+                    '--all-targets', '--all-features'
+                }
+            }
+        }
+    }
+}
 
 lsp.setup()
